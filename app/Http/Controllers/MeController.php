35cae8ffaf87572312e23895699047c66a94433f
DmitryNestor <dimchik.livethe5@gmail.com>
2023-12-09 15:46:33 +0600

    second commit

--- app/Http/Controllers/MeController.php
+++ app/Http/Controllers/MeController.php
@@ -9,10 +9,7 @@ class MeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $user = User::find(1);
-        $user->name = $request->get('name', default: 'Noname');
-        $user->save();
-        return 'hello epta, '. $request->get('name', default: 'Noname');
+
+        return auth()->user();
     }
 }
-
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MeController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/registration', RegisterController::class);
+    Route::post('/login', LoginController::class);
+});
+
+// Rooms
+Route::group(['prefix' => 'rooms', 'middleware' => 'auth:sanctum'], function () {
+    Route::post('{room}/step/', [RoomController::class, 'createStep']);
+    Route::resource('/', RoomController::class);
+    Route::post('/enter/{room}', [RoomController::class, 'enter']);
+    Route::post('/leave/{room}', [RoomController::class, 'leave']);
+    Route::get('/get-updates/{room}', [RoomController::class, 'getUpdates']);
+});
+
+Route::get('/me', MeController::class)->middleware('auth:sanctum');
